629649050bafbf33cae98147368c21abd1f5a714
150Balbes <Leo-sp150@yandex.ru>
2016-07-29 23:02:06 +0300

    edit script s905_autoscript 	modified:   config/bootscripts/boot-vegas95.cmd 	deleted:    config/bootscripts/boot-vegas95.dtb 	modified:   config/sources/vegas95.conf

--- config/bootscripts/boot-vegas95.cmd
+++ config/bootscripts/boot-vegas95.cmd
@@ -7,10 +7,9 @@ setenv initrd_loadaddr "0x13000000"
 setenv condev "console=ttyS0,115200n8 console=tty0 consoleblank=0"
 setenv bootargs "root=LABEL=ROOTFS rootwait rootflags=data=writeback rw ${condev} no_console_suspend hdmimode=${m} m_bpp=${m_bpp} fsck.repair=yes net.ifnames=0"
 setenv boot_start booti ${loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
-setenv load_dtb 'store dtb read $dtb_mem_addr; fdt addr $dtb_mem_addr'
-if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if load_dtb; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
+if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
 if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
-if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if load_dtb; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
+if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
 if fatload mmc 0:1 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
 # Recompile with:
 # mkimage -C none -A arm -T script -d /boot/s905_autoscript.cmd /boot/s905_autoscript
