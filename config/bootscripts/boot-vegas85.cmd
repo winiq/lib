bd415c1a3eb8911ac04b2b24f4f74730ec30a898
150Balbes <Leo-sp150@yandex.ru>
2016-09-14 17:34:23 +0300

    S805 add usb for s805_autoscript 	modified:   config/bootscripts/boot-vegas85.cmd

--- config/bootscripts/boot-vegas85.cmd
+++ config/bootscripts/boot-vegas85.cmd
@@ -9,6 +9,6 @@ setenv initrd_loadaddr "0x22000000"
 setenv boot_start bootm ${loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
 if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
 if fatload mmc 0:1 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${loadaddr} uImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
+if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
+if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} uImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
 
-#if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
-#if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} uImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
