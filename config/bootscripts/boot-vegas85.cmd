d14a9d8e0e89a2e99f23d263a3cdc694590bdaa7
150Balbes <Leo-sp150@yandex.ru>
2016-09-14 17:32:35 +0300

    S805 edit s805_autoscript 	modified:   config/bootscripts/boot-vegas85.cmd

--- config/bootscripts/boot-vegas85.cmd
+++ config/bootscripts/boot-vegas85.cmd
@@ -7,7 +7,8 @@ setenv loadaddr "0x21000000"
 setenv dtb_loadaddr "0x21800000"
 setenv initrd_loadaddr "0x22000000"
 setenv boot_start bootm ${loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
-if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
-if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
-if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
-if fatload mmc 0:1 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
+if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
+if fatload mmc 0:1 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${loadaddr} uImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
+
+#if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
+#if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} uImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
