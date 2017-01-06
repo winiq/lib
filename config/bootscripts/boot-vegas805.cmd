909e89735e727192421bb5d4f49f7eafc9a636a5
150Balbes <Leo-sp150@yandex.ru>
2017-01-06 15:43:23 +0300

    S812 ubuntu 	изменено:                   config/boards/vegas812.conf 	изменено:                   config/bootscripts/boot-vegas805.cmd 	изменено:                   config/bootscripts/boot-vegas812.cmd 	изменено:                   config/kernel/linux-vegas812-default.config 	изменено:                   config/sources/vegas812.conf 	new file:                   patch/u-boot/u-boot-vegas812/ext4-boot-improvements.patch

--- config/bootscripts/boot-vegas805.cmd
+++ config/bootscripts/boot-vegas805.cmd
@@ -4,5 +4,5 @@ setenv kernel_loadaddr "0x14000000"
 setenv dtb_loadaddr "0x11800000"
 setenv initrd_loadaddr "0x15000000"
 setenv boot_start bootm ${kernel_loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
-if fatload mmc 0 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
-if fatload usb 0 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${kernel_loadaddr} uImage; then if fatload usb 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
+if fatload mmc 0 ${initrd_loadaddr} uInitrd; then if fatload mmc 0 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
+if fatload usb 0 ${initrd_loadaddr} uInitrd; then if fatload usb 0 ${kernel_loadaddr} uImage; then if fatload usb 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
--- config/bootscripts/boot-vegas812.cmd
+++ config/bootscripts/boot-vegas812.cmd
@@ -1,23 +1,10 @@
-setenv m "1080p"
-setenv vout_mode "hdmi"
-setenv m_bpp "32"
-setenv hpd "0"
-setenv cec "1"
-setenv disableuhs "disableuhs"
-setenv vpu "1"
-setenv hdmioutput "1"
-setenv condev "console=ttyS0,115200n8 console=tty0 consoleblank=0"
-setenv hdmi_hpd "disablehpd=true"
-setenv hdmi_cec "hdmitx=cecf"
-setenv bootargs "root=LABEL=ROOTFS rootwait ro ${condev} loglevel=1  no_console_suspend vdaccfg=0xa000 logo=osd1,loaded,0x7900000,720p,full dmfc=3 cvbsmode=576cvbs hdmimode=${m} m_bpp=${m_bpp} vout=${vout_mode} ${disableuhs} ${hdmi_hpd} ${hdmi_cec}"
-setenv loadaddr "0x21000000"
-setenv dtb_loadaddr "0x21800000"
-setenv initrd_loadaddr "0x22000000"
-setenv boot_start bootm ${loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
-fdt addr 21800000
-if fatload usb 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
-if fatload usb 0:1 ${initrd_loadaddr} uInitrd; then if fatload usb 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
-if fatload mmc 0:1 ${dtb_loadaddr} dtb.img; then setenv dtb_img "1"; else if store dtb read $dtb_loadaddr; then setenv dtb_img "1"; else setenv dtb_img "0";fi;fi;
-if fatload mmc 0:1 ${initrd_loadaddr} uInitrd; then if fatload mmc 0:1 ${loadaddr} zImage; then if test "${dtb_img}" = "1"; then run boot_start;fi;fi;fi;
+setenv condev "console=ttyS0,115200n8 console=tty0 no_console_suspend consoleblank=0"
+setenv bootargs "root=LABEL=ROOTFS rootflags=data=writeback rw ${condev} fsck.repair=yes net.ifnames=0"
+setenv kernel_loadaddr "0x14000000"
+setenv dtb_loadaddr "0x11800000"
+setenv initrd_loadaddr "0x15000000"
+setenv boot_start bootm ${kernel_loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
+if fatload usb 0 ${initrd_loadaddr} uInitrd; then if fatload usb 0 ${kernel_loadaddr} uImage; then if fatload usb 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
+if fatload mmc 0 ${initrd_loadaddr} uInitrd; then if fatload mmc 0 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
 
-#setenv bootargs "root=LABEL=ROOTFS rootwait rootflags=data=writeback rw ${condev} no_console_suspend hdmimode=${m} m_bpp=${m_bpp} fsck.repair=yes net.ifnames=0"
+#setenv bootargs "root=LABEL=ROOTFS rw ${condev} logo=osd1,loaded,0x7900000,720p,full hdmimode=${m} m_bpp=${m_bpp} mac=${mac}"
