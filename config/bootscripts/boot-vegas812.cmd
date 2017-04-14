a55bfc6985f0027e5b25f64542ef79f8f5073683
balbes150 <balbes-150@yandex.ru>
2017-04-14 14:04:32 +0300

    s812 fix

--- config/bootscripts/boot-vegas812.cmd
+++ config/bootscripts/boot-vegas812.cmd
@@ -4,7 +4,7 @@ setenv kernel_loadaddr "0x14000000"
 setenv dtb_loadaddr "0x11800000"
 setenv initrd_loadaddr "0x15000000"
 setenv boot_start bootm ${kernel_loadaddr} ${initrd_loadaddr} ${dtb_loadaddr}
-if fatload usb 0 ${initrd_loadaddr} uInitrd; then if fatload usb 0 ${kernel_loadaddr} uImage; then if fatload usb 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
-if fatload mmc 0 ${initrd_loadaddr} uInitrd; then if fatload mmc 0 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_loadaddr} dtb.img; then run boot_start;fi;fi;fi;
+if fatload usb 0 ${initrd_loadaddr} uInitrd; then if fatload usb 0 ${kernel_loadaddr} uImage; then if fatload usb 0 ${dtb_loadaddr} dtb.img; then run boot_start; else imgread dtb boot ${loadaddr} ${dtb_loadaddr}; run boot_start;fi;fi;fi;
+if fatload mmc 0 ${initrd_loadaddr} uInitrd; then if fatload mmc 0 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_loadaddr} dtb.img; then run boot_start; else imgread dtb boot ${loadaddr} ${dtb_loadaddr}; run boot_start;fi;fi;fi;
 
 #setenv bootargs "root=LABEL=ROOTFS rw ${condev} logo=osd1,loaded,0x7900000,720p,full hdmimode=${m} m_bpp=${m_bpp} mac=${mac}"
