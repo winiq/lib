b47a78e61e45ec61967cc0ec82a350fba5244bed
balbes150 <balbes-150@yandex.ru>
2017-04-14 17:30:46 +0300

    add mate-aplets 	изменено:      configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -149,7 +149,7 @@ case $BUILD_DESKTOP_DE in
 	PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP xfce4 xfce4-screenshooter xfce4-notifyd"
 	;;
 	mate)
-	PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mate-desktop-environment-extras mate-media mate-screensaver mate-utils mate-power-manager"
+	PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mate-desktop-environment-extras mate-media mate-screensaver mate-utils mate-power-manager mate-applets"
 	;;
 esac
 
